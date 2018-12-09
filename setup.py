a311474950fb79dcfc290c56f4a2520db7420306
Cesar <CeL124@users.noreply.github.com>
2018-12-09 00:36:24 -0500

    Update setup.py
    add description to setup file

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from distutils.core import setup
 setup(
     name='svhndata',
     version='1.0',
-    packages=['svhndata']
+    packages=['svhndata'],
+    description = ("Package will download, load, and format SVHN dataset for machine learning modeling")
 )
